46895189a267d48bb901ec9104eaa25e147c1b51
Artem Shalamov <iart1756@gmail.com>
2020-05-04 12:33:11 +0500

    data output

--- routes/food.js
+++ routes/food.js
@@ -1,10 +1,13 @@
 const {Router} = require('express')
+const Foods = require('../models/food')
 const router = Router()
 
 router.get('/', async (req, res) => {
+    const foods = await Foods.getAll()
     res.render('food', {
         title: 'Готовые блюда',
         isFood: true,
+        foods
     })
 })
 
